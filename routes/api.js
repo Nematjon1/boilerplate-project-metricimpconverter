7a7a0fcc52c5de8be046d1b3b5f139cffc1acfd3
Nematjon <frank.sport.93@gmail.com>
2021-01-17 15:21:23 +0300

    API complete

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,32 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
-      let input = req.query.input;
-      let initNum = convertHandler.getNum(input);
-      let initUnit = convertHandler.getUnit(input);
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        let input = req.query.input;
+        let initNum = convertHandler.getNum(input);
+        let initUnit = convertHandler.getUnit(input);
+
+        if(initNum === "invalid number" && initUnit === "invalid unit") {
+            res.send('invalid number and unit');
+        } else if(initNum === "invalid number") {
+            res.send(initNum);
+        } else if(initUnit === "invalid unit") {
+            res.send(initUnit);
+        } else {
+            let returnNum = convertHandler.convert(initNum, initUnit);
+            let returnUnit = convertHandler.getReturnUnit(initUnit);
+            let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+            console.log("Res: ", input, initNum, initUnit, returnNum, returnUnit, toString);
+                    //res.json
+            res.json({
+                initNum,
+                initUnit: initUnit === "l" ? "L" : initUnit === "L" ?"L" : initUnit.toLowerCase(),
+                returnNum,
+                returnUnit,
+                string: toString
+            });
+        }
       
-      //res.json
     });
     
 };
